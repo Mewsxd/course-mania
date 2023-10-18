88681dd1f8e537f2a67bc9055e557b478149e41e
Muzammil Malik <abdmalik1593@gmail.com>
2023-10-18 20:41:33 +0530

    Added scroll to top again after accidentally deleteing

--- src/LayoutComponent.js
+++ src/LayoutComponent.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
+import ScrollToTop from "./ScrollToTop";
 
 const LayoutComponent = () => {
   return (
     <div>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
     </div>
